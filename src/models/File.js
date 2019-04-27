657830cde976e59133663f978f750b203410fddc
Felipe B. Netto <felipe.netto@mobiauto.com.br>
2019-04-27 02:29:46 -0300

    file url listening to environment variable

--- src/models/File.js
+++ src/models/File.js
@@ -16,9 +16,7 @@ const File = new mongoose.Schema({
 });
 
 File.virtual("url").get(function(){
-    const url = process.env.URL || 'http://localhost:3000';
-
-    return `${url}/files/${encodeURIComponent(this.path)}`;
+    return `${process.env.URL}/files/${encodeURIComponent(this.path)}`;
 });
 
 module.exports = mongoose.model('File', File);
